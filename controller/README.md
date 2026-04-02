82ede63282a50bbc0f067a21dabf1a9880c479f7
debater-coder <hamzah@syedahmed.net>
2026-04-02 19:00:21 +1100

    redo controller

--- controller/README.md
+++ controller/README.md
@@ -2,31 +2,19 @@
 
 This is the central control software for the robot, running on a Raspberry Pi.
 It is responsible for:
+
 - vision (detections of field, ball and other robots),
 - sending actuator commands over serial
 - performing closed loop control to determine actuator commands based on a target value
 - computing target values based on a soccer strategy
 
 ## Infrastructure
+
 Dependencies are managed by the `uv` python package manager. This package manager itself
 is installed via a Nix flake.
 
 To run the flake shell, run inside of this folder:
+
 ```bash
 nix develop
-```
-
-The `uv` package manager will create its own `.venv` directory by default.
-However, Raspberry Pi specific dependencies such as `picamera2` depend on the
-versions libraries installed by `apt`. Thus, it is necessary to create a custom
-venv with `--system-site-packages` to include the python bindings to these
-libraries. `uv` will then manage the activated virtual environment.
-**Ensure the Nix DevShell is activated when creating the venv to avoid version conflicts**
-```bash
-python -m venv --system-site-packages .venv
-```
-
-Some dependencies (eg: opencv-python) require built dependencies better installed from apt:
-```bash
-sudo apt install python3-opencv
 ```
--- controller/main.py
+++ controller/main.py
@@ -1,36 +1,11 @@
-import cv2
-from picamera2 import Picamera2
+import numpy as np
+import rerun as rr
 
-height = 480
-width = 640
+FIELD_BOX_SIZE = np.array([2.43, 1.82, 0.22])
+PLAYING_BOX_SIZE = FIELD_BOX_SIZE - np.array([0.5, 0.5, 0])
 
-orangeLower = (0, 160, 120)
-orangeUpper = (20, 255, 255)
+rr.init("robocup")
+rr.connect_grpc()
 
-cam = Picamera2()
-
-cam.configure(
-    cam.create_video_configuration(main={"format": "XRGB8888", "size": (width, height)})
-)
-cam.start()
-
-while True:
-    frame = cv2.flip(cam.capture_array(), -1)
-
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, orangeLower, orangeUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    contours = cv2.findContours(
-        mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )[0]
-
-    if len(contours) > 0:
-        contour = max(contours, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(contour)
-        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 0, 255), 5)
-
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Mask", mask)
-    cv2.waitKey(1)
+rr.log("field/walls", rr.Boxes3D(sizes=FIELD_BOX_SIZE, colors=[0, 255, 0]))
+rr.log("field/playing_area", rr.Boxes3D(sizes=PLAYING_BOX_SIZE, colors=[255, 255, 255]))
