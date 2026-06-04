38932fedf55b2b4567b2ea33041d5eeb233ed215
debater-coder <hamzah@syedahmed.net>
2026-06-04 13:38:48 +1000

    fix

--- camera_streamer/main.py
+++ camera_streamer/main.py
@@ -28,7 +28,7 @@ frame_buffer = np.ndarray(frame_shape, buffer=frame_buffer_shm.buf, dtype=frame.
 try:
     while True:
         frame_buffer[:] = np.rot90(
-            cam.capture_array(), k=3, axes=(0, 1)
+            cam.capture_array(), k=2, axes=(0, 1)
         )  # in place updating of the buffer
         sleep(0.01)
 finally:
