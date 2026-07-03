339da00b9a6e5e5949112f4de7ceb1212ddf8705
debater-coder <hamzah@syedahmed.net>
2026-07-03 10:25:48 +1000

    feat: force forward when ball sensor detected

--- controller/behaviours/BallChaseBehaviour.py
+++ controller/behaviours/BallChaseBehaviour.py
@@ -35,15 +35,19 @@ class BallChaseBehaviour(py_trees.behaviour.Behaviour):
         if (centre := self.blackboard.ball_centre) and (
             radius := self.blackboard.ball_radius
         ):
-            if centre[0] > 300:
-                self.command.send_command(100, 0, 4, 1)
-                self.feedback_message = "turning right"
-            elif centre[0] < 180:
-                self.command.send_command(100, 0, -4, 1)
-                self.feedback_message = "turning left"
-            else:
+            if tof is not None and tof < 100:
                 self.command.send_command(200, 0, 0, 1)
                 self.feedback_message = "forwards"
+            else:
+                if centre[0] > 300:
+                    self.command.send_command(100, 0, 4, 1)
+                    self.feedback_message = "turning right"
+                elif centre[0] < 180:
+                    self.command.send_command(100, 0, -4, 1)
+                    self.feedback_message = "turning left"
+                else:
+                    self.command.send_command(200, 0, 0, 1)
+                    self.feedback_message = "forwards"
 
         else:
             self.command.send_command(0, 0, 10, 0)
