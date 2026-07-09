8b322f3334c563c8f1851d6c10eb9730249224d9
debater-coder <hamzah@syedahmed.net>
2026-07-09 14:18:29 +1000

    fix flake

--- controller/flake.nix
+++ controller/flake.nix
@@ -1,12 +1,11 @@
 {
-  description = "Robot Soccer Controller for Raspberry Pi";
+  description = "GPIO Zero uv dev shell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
@@ -23,18 +22,28 @@
               pkgs.uv
               pkgs.cmake
               pkgs.python314
-              pkgs.stdenv.cc.cc
-              pkgs.zlib
-              pkgs.glibc
+              pkgs.pkg-config
               pkgs.swig
+
+              pkgs.stdenv.cc
+              pkgs.stdenv.cc.cc.lib
+
+              pkgs.zlib
+              pkgs.glibc.dev
               pkgs.lgpio
             ];
+
             shellHook = ''
-              	    export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-              			export GPIOZERO_PIN_FACTORY=lgpio
-                    export CFLAGS="-I${pkgs.lgpio}/include $CFLAGS"
-                    export LDFLAGS="-L${pkgs.lgpio}/lib $LDFLAGS"
-              	  '';
+              export GPIOZERO_PIN_FACTORY=lgpio
+
+              export CC=cc
+              export CXX=c++
+
+              export CFLAGS="-I${pkgs.lgpio}/include $CFLAGS"
+              export LDFLAGS="-L${pkgs.lgpio}/lib $LDFLAGS"
+
+		export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.lgpio}/lib:$LD_LIBRARY_PATH"
+            '';
           };
         }
       );
