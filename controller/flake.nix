0c34ad4ad24ea3cb2f00de06dbe68a99eb19ba43
debater-coder <hamzah@syedahmed.net>
2026-07-09 13:57:19 +1000

    fix flake

--- controller/flake.nix
+++ controller/flake.nix
@@ -22,14 +22,19 @@
             packages = [
               pkgs.uv
               pkgs.cmake
-	      pkgs.python314
-	      pkgs.stdenv.cc.cc
-    	      pkgs.zlib
+              pkgs.python314
+              pkgs.stdenv.cc.cc
+              pkgs.zlib
               pkgs.glibc
+              pkgs.swig
+              pkgs.lgpio
             ];
-	shellHook = ''
-	    export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-	  '';
+            shellHook = ''
+              	    export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+              			export GPIOZERO_PIN_FACTORY=lgpio
+                    export CFLAGS="-I${pkgs.lgpio}/include $CFLAGS"
+                    export LDFLAGS="-L${pkgs.lgpio}/lib $LDFLAGS"
+              	  '';
           };
         }
       );
