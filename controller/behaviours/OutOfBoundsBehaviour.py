32232f35ca32f0e7b758db6cb6120e7fae16206b
debater-coder <hamzah@syedahmed.net>
2026-07-11 20:07:00 +1000

    fix: slow down out of bounds

--- controller/behaviours/OutOfBoundsBehaviour.py
+++ controller/behaviours/OutOfBoundsBehaviour.py
@@ -24,27 +24,27 @@ class OutOfBoundsBehaviour(py_trees.behaviour.Behaviour):
         match line_status:
             case 1:
                 self.feedback_message = "Going right"
-                self.command.send_command(0, -300, 0, 1)
+                self.command.send_command(0, -100, 0, 1)
                 return py_trees.common.Status.RUNNING
             case 2:
                 self.feedback_message = "Going forwards"
-                self.command.send_command(0, 300, 0, 1)
+                self.command.send_command(0, 100, 0, 1)
                 return py_trees.common.Status.RUNNING
             case 3:
                 self.feedback_message = "Going forwards right"
-                self.command.send_command(200, -200, 0, 1)
+                self.command.send_command(100, -100, 0, 1)
                 return py_trees.common.Status.RUNNING
             case 4:
                 self.feedback_message = "Going left"
-                self.command.send_command(0, 300, 0, 1)
+                self.command.send_command(0, 100, 0, 1)
                 return py_trees.common.Status.RUNNING
             case 5:
                 self.feedback_message = "Going backwards"
-                self.command.send_command(-300, 0, 0, 1)
+                self.command.send_command(-100, 0, 0, 1)
                 return py_trees.common.Status.RUNNING
             case 6:
                 self.feedback_message = "Going forwards left"
-                self.command.send_command(200, 200, 0, 1)
+                self.command.send_command(100, 100, 0, 1)
                 return py_trees.common.Status.RUNNING
 
         if line_status != 0:
