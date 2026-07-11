e5abae4ef521498e0fe20b651bee1327b46bb6c7
debater-coder <hamzah@syedahmed.net>
2026-07-11 20:24:13 +1000

    fix

--- controller/behaviours/OutOfBoundsBehaviour.py
+++ controller/behaviours/OutOfBoundsBehaviour.py
@@ -1,3 +1,4 @@
+import time
 import typing
 
 import py_trees
@@ -8,6 +9,7 @@ from protocols.command import SupportsCommand
 class OutOfBoundsBehaviour(py_trees.behaviour.Behaviour):
     def __init__(self, name: str):
         super().__init__(name)
+        self.timeout = None
 
     def setup(self, **kwargs: typing.Any) -> None:
         if "command" in kwargs and isinstance(
@@ -24,30 +26,29 @@ class OutOfBoundsBehaviour(py_trees.behaviour.Behaviour):
         match line_status:
             case 1:
                 self.feedback_message = "Going right"
-                self.command.send_command(0, -100, 0, 1)
-                return py_trees.common.Status.RUNNING
+                self.timeout = (time.time(), (0, -100, 0, 1))
             case 2:
                 self.feedback_message = "Going forwards"
-                self.command.send_command(0, 100, 0, 1)
-                return py_trees.common.Status.RUNNING
+                self.timeout = (time.time(), (0, 100, 0, 1))
             case 3:
                 self.feedback_message = "Going forwards right"
-                self.command.send_command(100, -100, 0, 1)
-                return py_trees.common.Status.RUNNING
+                self.timeout = (time.time(), (100, -100, 0, 1))
             case 4:
                 self.feedback_message = "Going left"
-                self.command.send_command(0, 100, 0, 1)
-                return py_trees.common.Status.RUNNING
+                self.timeout = (time.time(), (0, 100, 0, 1))
             case 5:
                 self.feedback_message = "Going backwards"
-                self.command.send_command(-100, 0, 0, 1)
-                return py_trees.common.Status.RUNNING
+                self.timeout = (time.time(), (-100, 0, 0, 1))
             case 6:
                 self.feedback_message = "Going forwards left"
-                self.command.send_command(100, 100, 0, 1)
-                return py_trees.common.Status.RUNNING
+                self.timeout = (time.time(), (100, 100, 0, 1))
+
+        if (timeout := self.timeout) and time.time() - timeout[0] < 2:
+            self.command.send_command(*timeout[1])
+            return py_trees.common.Status.RUNNING
 
         if line_status != 0:
             self.feedback_message = "Invalid line sensor state"
+
         self.feedback_message = "No line detected"
         return py_trees.common.Status.FAILURE
