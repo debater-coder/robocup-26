fac1ff45733df0eb541b54eb079aa5db0f065e6e
debater-coder <hamzah@syedahmed.net>
2026-07-20 20:31:46 +1000

    fix: add controler

--- enable-services.sh
+++ enable-services.sh
@@ -1,3 +1,5 @@
 systemctl enable ./robocup-camera.service
+systemctl enable ./robocup-controller.service
 sudo systemctl daemon-reload
 sudo systemctl start robocup-camera
+sudo systemctl start robocup-controller
