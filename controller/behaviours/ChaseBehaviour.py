199f4e137dbab26f6a6a5118a98c0f3c2709c1ea
debater-coder <hamzah@syedahmed.net>
2026-07-25 13:58:14 +1000

    e

--- controller/behaviours/ChaseBehaviour.py
+++ controller/behaviours/ChaseBehaviour.py
@@ -28,10 +28,10 @@ class ChaseBehaviour(py_trees.behaviour.Behaviour):
 
     def update(self):
         if centre := self.blackboard.target:
-            if centre[0] > 612:
+            if centre[0] > 875:
                 self.command.send_command(200, 0, 4, 1)
                 self.feedback_message = "turning right"
-            elif centre[0] < 412:
+            elif centre[0] < 725:
                 self.command.send_command(200, 0, -4, 1)
                 self.feedback_message = "turning left"
             else:
