08c1291fde0ac1f86fddaae37b0c9b1db54c30e4
debater-coder <hamzah@syedahmed.net>
2026-07-30 14:09:54 +1000

    e

--- controller/calibrate.py
+++ controller/calibrate.py
@@ -53,8 +53,9 @@ while len(objpoints) < 20:
         imgpoints.append(corners2)
 
         print(f"Successfully captured frame {len(objpoints)}/20")
+        sleep(1)  # wait for new frame
+    sleep(0.02)
 
-    sleep(1)  # wait for new frame
 
 h, w = gray.shape
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
