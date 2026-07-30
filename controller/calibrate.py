8cb58eb10970592a8a457ac2f9e01e91674c0ce5
debater-coder <hamzah@syedahmed.net>
2026-07-30 14:24:58 +1000

    e

--- controller/calibrate.py
+++ controller/calibrate.py
@@ -30,7 +30,7 @@ CRITERIA = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((10 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+objp[:, :2] = np.mgrid[0:10, 0:7].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
