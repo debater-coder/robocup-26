79933f37cc506d6dd30e0cbb37a69bca899dfc65
debater-coder <hamzah@syedahmed.net>
2026-08-18 11:08:50 +1000

    wip

--- controller/calibrate.py
+++ controller/calibrate.py
@@ -47,10 +47,10 @@ while len(objpoints) < 20:
     # If found, add object points, image points (after refining them)
     if ret:
         # The object points (coordinates of checkerboard points in 3D) remain the same for each frame
-        objpoints.append(np.reshape(objp, (1, -1, 3)))
+        objpoints.append(np.reshape(objp, (1, -1, 3)).astype(np.float32))
 
         refined_corners = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), CRITERIA)
-        imgpoints.append(np.reshape(refined_corners, (-1, 1, 2)))
+        imgpoints.append(np.reshape(refined_corners, (1, -1, 2).astype(np.float32)))
 
         rr.log("frame", rr.Image(frame).compress(jpeg_quality=50))
         rr.log("gray", rr.Image(gray).compress(jpeg_quality=50))
