8154b1814ec1f6d2350c93677a409030611447ff
debater-coder <hamzah@syedahmed.net>
2026-08-18 14:05:39 +1000

    wip

--- enable-services.sh
+++ enable-services.sh
@@ -1,5 +1,7 @@
 systemctl enable ./robocup-camera.service
 systemctl enable ./robocup-controller.service
+systemctl enable ./robocup-ui.service
 sudo systemctl daemon-reload
+sudo systemctl start robocup-ui
 sudo systemctl start robocup-camera
 sudo systemctl start robocup-controller
