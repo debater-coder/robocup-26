4d1ead5b1c72d0e27fe220163aa078322ad3cac1
debater-coder <hamzah@syedahmed.net>
2026-08-21 12:41:31 +1000

    e

--- controller/vision/vision.py
+++ controller/vision/vision.py
@@ -70,9 +70,9 @@ ground_matrix = (
 
 def transform_array(arr):
     """Transforms an array of points of shape (N, 2) from image coordinates to plane coordinates (in mm)"""
-    points = cv2.fisheye.undistortPoints(arr, camera_matrix, dist_coeffs).reshape(
-        (-1, 2)
-    )
+    points = cv2.fisheye.undistortPoints(
+        arr.astype(np.float64), camera_matrix, dist_coeffs
+    ).reshape((-1, 2))
     points = np.hstack([arr, np.ones((len(arr), 1))])  # make into 3D arrays (X, Y, 1)
     projected = (
         points @ ground_matrix.T
