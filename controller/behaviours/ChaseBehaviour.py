2b89d614c040861607c5534810f0b69983abffdf
debater-coder <hamzah@syedahmed.net>
2026-08-24 11:08:08 +1000

    tune]

--- controller/behaviours/ChaseBehaviour.py
+++ controller/behaviours/ChaseBehaviour.py
@@ -37,7 +37,7 @@ class ChaseBehaviour(py_trees.behaviour.Behaviour):
 
         self.pid_x = PID(3, 0, 0, setpoint=0)
         self.pid_y = PID(3, 0, 0, setpoint=0)
-        self.pid_theta = PID(-10, 0, -5, setpoint=0, output_limits=(-20, 20))
+        self.pid_theta = PID(-5, 0, -0.1, setpoint=0, output_limits=(-20, 20))
 
     def setup(self, **kwargs: typing.Any) -> None:
         if "command" in kwargs and isinstance(
@@ -63,6 +63,7 @@ class ChaseBehaviour(py_trees.behaviour.Behaviour):
             vel_theta = self.pid_theta(theta)
 
             rr.log("/chase/theta/measured", rr.Scalars(theta))
+            rr.log("/chase/theta/out", rr.Scalars(vel_theta))
             rr.log("/chase/theta/p", rr.Scalars(self.pid_theta.components[0]))
             rr.log("/chase/theta/i", rr.Scalars(self.pid_theta.components[1]))
             rr.log("/chase/theta/d", rr.Scalars(self.pid_theta.components[2]))
